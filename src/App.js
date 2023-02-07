4431e1e96aa65ed5acdd7a28ee411aab0dc2abd0
Taraansh <taraanshnandwani123@gmail.com>
2023-02-08 00:19:53 +0530

    Added Login Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
 import './App.css';
+import Login from './components/Login';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
     <NavBar/>
+    <Login/>
     </>
   );
 }
