0a4f08cd856770c406511c0e2a88a8f21ae1b8b2
Taraansh <taraanshnandwani123@gmail.com>
2023-02-08 00:18:43 +0530

    NavBar modified

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,29 +2,21 @@ import React from 'react'
 import logo from './logo.png';
 const NavBar = () => {
   return (
-    <div>
-      <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
-  <a className="navbar-brand" href="/"><img src={logo} width="70" height="40" className="d-inline-block align-top" alt=""/></a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarNav">
-    <ul className="navbar-nav">
-      <li className="nav-item active">
-        <a className="nav-link" href="/">Home </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/">Features</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/">Pricing</a>
-      </li>
-     
-    </ul>
-
-  </div>
-</nav>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="/"><img src={logo} width="70" height="30" className="d-inline-block align-top" alt=""/></a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+          <a className="nav-link" href="/">Features</a>
+          <a className="nav-link" href="/">Pricing</a>
+        </div>
+      </div>
     </div>
+  </nav>
   )
 }
 
