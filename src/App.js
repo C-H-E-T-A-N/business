1d6824e45f4b960bb5f8e09031d272681a5b5983
CHETAN SHARMA <chetansharma.pvt@gmail.com>
2023-02-07 23:07:15 +0530

    new page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
-
+import Page01 from './components/Page01';
 import NavBar from './components/NavBar';
 function App() {
   return (
     <>
     <NavBar/>
+    <Page01/>
     </>
   );
 }
