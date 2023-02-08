4427f7d4c4c4da5b63ce6c43839efa3c53252b57
Taraansh <taraanshnandwani123@gmail.com>
2023-02-09 00:15:23 +0530

    Added Router

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -9,11 +9,11 @@ export default function Signup() {
       <div className="card-body">
       <form>
       <div className="mb-3">
-        <label for="exampleInputEmail1" className="form-label">Email address</label>
+        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
       </div>
       <div className="mb-3">
-        <label for="exampleInputPassword1" className="form-label">Password</label>
+        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
         <input type="password" className="form-control" id="exampleInputPassword1"/>
       </div>  
       <button type="submit" className="btn btn-primary d-grid gap-2 col-12 mx-auto" style={{marginTop: '30px'}}>SignUp</button>
