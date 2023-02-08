b4306c462e5db3c4565d8f83384d8eb43afbb4c9
Taraansh <taraanshnandwani123@gmail.com>
2023-02-09 00:13:40 +0530

    Added router

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import AddBn from "./AddBn";
 
 export default function Login() {
   return (
     <>
+    {/* <AddBn/> */}
     <div className="card mx-auto position-absolute top-50 start-50 translate-middle" style={{width: '18rem', padding: '35px 20px'}}>
         <h5 className="card-title">Login</h5>
       <div className="card-body">
       <form>
       <div className="mb-3">
-        <label for="exampleInputEmail1" className="form-label">Email address</label>
+        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
       </div>
       <div className="mb-3">
-        <label for="exampleInputPassword1" className="form-label">Password</label>
+        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
         <input type="password" className="form-control" id="exampleInputPassword1"/>
       </div>  
-      <button type="submit" className="btn btn-primary d-grid gap-2 col-12 mx-auto" style={{marginTop: '30px'}}>Login</button>
+      <div className="btn btn-primary d-grid gap-2 col-12 mx-auto" onClick={<AddBn/>} style={{marginTop: '30px'}}><Link to="/AddBn" style={{textDecoration: 'none', color: 'white'}}>Login</Link>
+</div>
     </form>  
     </div>
         <div id="newEmailSetUp" className="form-text mx-3">Need an accout? <Link to="/Signup">Sign up</Link> </div>
