7b1d690f9b130186ac4a35dca50b8044fcb2a731
Taraansh <taraanshnandwani123@gmail.com>
2023-02-08 22:34:38 +0530

    Added textboxes

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,13 +3,23 @@ import React from "react";
 export default function Login() {
   return (
     <>
-<div className="card mx-auto position-absolute top-50 start-50 translate-middle" style={{width: '18rem', padding: '60px 30px'}}>
-  <div className="card-body">
-    <h5 className="card-title">Special title treatment</h5>
-    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="/" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+    <div className="card mx-auto position-absolute top-50 start-50 translate-middle" style={{width: '18rem', padding: '35px 20px'}}>
+        <h5 className="card-title">Login</h5>
+      <div className="card-body">
+      <form>
+      <div className="mb-3">
+        <label for="exampleInputEmail1" className="form-label">Email address</label>
+        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+      </div>
+      <div className="mb-3">
+        <label for="exampleInputPassword1" className="form-label">Password</label>
+        <input type="password" className="form-control" id="exampleInputPassword1"/>
+      </div>  
+      <button type="submit" className="btn btn-primary d-grid gap-2 col-12 mx-auto" style={{marginTop: '30px'}}>Login</button>
+    </form>  
+    </div>
+        <div id="newEmailSetUp" className="form-text mx-3">Need an accout? <a href="/">Sign up</a> </div>
+    </div>
     </>
   );
 }
