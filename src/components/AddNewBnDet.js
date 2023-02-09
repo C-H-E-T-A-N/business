4c11c92c01b7f911d762a1aea11e6f62e17b8cfc
Taraansh <taraanshnandwani123@gmail.com>
2023-02-09 23:52:14 +0530

    Created new component to add details of new business

---
+++ src/components/AddNewBnDet.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AddNewBnDet() {
+  return (
+    <div>
+      <div className="container">
+        <h1 className='my-3'>New business Details</h1>
+      </div>
+    </div>
+  )
+}
