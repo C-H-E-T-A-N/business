9a01a186d3d55e05d092e6b79e2f755557453455
Taraansh <taraanshnandwani123@gmail.com>
2023-02-09 23:52:53 +0530

    Created new component to show existing business

---
+++ src/components/ExiBn.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ExiBnItem from './ExiBnItem'
+
+export default function ExiBn() {
+  return (
+    <>
+    <div className="container">
+        <h1 className='my-3'>Existing Business</h1> 
+
+        Will continue after database
+        <ExiBnItem/>
+               
+    </div> 
+    </>
+  )
+}
