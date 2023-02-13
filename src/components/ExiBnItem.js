12d2486ef09082ca6491d1d3a62047ca8d84609c
Taraansh <taraanshnandwani123@gmail.com>
2023-02-13 23:42:46 +0530

    Removed a href

--- src/components/ExiBnItem.js
+++ src/components/ExiBnItem.js
@@ -1,16 +1,13 @@
 import React from 'react'
-import Login from './Login'
+import ExiBnDt from './ExiBnDt';
+
 
 export default function ExiBnItem() {
   return (
     <div>
-    <a href='/' style={{textDecoration: 'none', color: 'black'}}>
-        <div className="card my-2">
-        <div className="card-body" onClick={()=>{<Login/>}}>
+        <div className="card my-2 card-body text-black" onClick={()=>{<ExiBnDt/>}}>
             {/* Sr.no and name passed as prop*/} Name of business
         </div>
-        </div>
-    </a>
     </div>
   )
 }
