502e38b14e81aa3d2c83524c112d895ebf27ea7d
CHETAN SHARMA <chetansharma.pvt@gmail.com>
2023-02-13 21:49:44 +0530

    new component added exibndt in react router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css'
 import NavBar from './components/NavBar';
 import Login from './components/Login';
 import Signup from './components/Signup';
-import AddBn from "./components/AddBn"
+import AddBn from "./components/AddBn";
+import ExiBnDt from './components/ExiBnDt';
+
 import {
   BrowserRouter,
   Routes,
@@ -12,10 +14,12 @@ import {
 function App() {
   return (
     <>
-    <BrowserRouter>
     <NavBar/>
+    <BrowserRouter>
+    
       <Routes>
         <Route path="/" element={<Login/>} />
+        <Route path="/ExiBnDt" element={<ExiBnDt/>} />
         <Route path="/Login" element={<Login/>} />
         <Route path="/SignUp" element={<Signup/>} />
         <Route path='/AddBn' element={<AddBn/>}/>
